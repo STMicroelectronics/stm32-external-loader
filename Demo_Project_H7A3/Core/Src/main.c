ce2c14e024b4cceca72123dc4f1598fdb79a76d0
Chris Idema <48433711+ChrisIdema@users.noreply.github.com>
2023-01-09 18:31:17 +0100

    intermediate commit

--- Demo_Project_H7A3/Core/Src/main.c
+++ Demo_Project_H7A3/Core/Src/main.c
@@ -23,7 +23,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include <string.h> // for memcmp
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -55,6 +55,7 @@ void SystemClock_Config(void);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
+static uint8_t buffer_test[MEMORY_SECTOR_SIZE];
 /* USER CODE END 0 */
 
 /**
@@ -90,7 +91,47 @@ int main(void)
   MX_GPIO_Init();
   MX_OCTOSPI1_Init();
   /* USER CODE BEGIN 2 */
+	
+  	uint32_t var = 0;
 
+  	CSP_QUADSPI_Init();
+
+  	for (var = 0; var < MEMORY_SECTOR_SIZE; var++) {
+  		buffer_test[var] = (var & 0xff);
+  	}
+
+  	for (var = 0; var < SECTORS_COUNT; var++) {
+
+  		if (CSP_QSPI_EraseSector(var * MEMORY_SECTOR_SIZE,
+  				(var + 1) * MEMORY_SECTOR_SIZE - 1) != HAL_OK) {
+
+  			while (1)
+  				;  //breakpoint - error detected
+  		}
+
+  		if (CSP_QSPI_WriteMemory(buffer_test, var * MEMORY_SECTOR_SIZE,
+  				sizeof(buffer_test)) != HAL_OK) {
+
+  			while (1)
+  				;  //breakpoint - error detected
+  		}
+
+  	}
+
+  	if (CSP_QSPI_EnableMemoryMappedMode() != HAL_OK) {
+
+  		while (1)
+  			; //breakpoint - error detected
+  	}
+
+  	for (var = 0; var < SECTORS_COUNT; var++) {
+  		if (memcmp(buffer_test,
+  				(uint8_t*) (0x90000000 + var * MEMORY_SECTOR_SIZE),
+  				MEMORY_SECTOR_SIZE) != HAL_OK) {
+  			while (1)
+  				;  //breakpoint - error detected - otherwise QSPI works properly
+  		}
+  	}
   /* USER CODE END 2 */
 
   /* Infinite loop */

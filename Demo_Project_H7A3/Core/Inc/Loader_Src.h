0a3bb957c75ddbfb7ffa5ebba4954608724e934d
Chris Idema <cidema@hedon.nl>
2023-01-19 11:38:06 +0100

    enabled Discard unused sections (53.38kiB to 15.29kiB)

--- Demo_Project_H7A3/Core/Inc/Loader_Src.h
+++ Demo_Project_H7A3/Core/Inc/Loader_Src.h
@@ -1,8 +1,10 @@
 #ifndef INC_LOADER_SRC_H_
 #define INC_LOADER_SRC_H_
 
-
 #include <stdint.h>
+
+#define LOADER_OK	0x1
+#define LOADER_FAIL	0x0
 
 int Init(void);
 int SectorErase(uint32_t EraseStartAddress, uint32_t EraseEndAddress);
@@ -10,6 +12,5 @@ int MassErase(void);
 int Write(uint32_t Address, uint32_t Size, const uint8_t* buffer);
 uint32_t CheckSum(uint32_t StartAddress, uint32_t Size, uint32_t InitVal);
 uint64_t Verify(uint32_t MemoryAddr, uint32_t RAMBufferAddr, uint32_t Size,uint32_t missalignement);
-
 
 #endif // INC_LOADER_SRC_H_
